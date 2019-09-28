a86d39a0db181335c935642345e614c3e8edc8b1
Arthur Shih <a1928375@hotmail.com>
2019-09-28 14:28:10 +0800

    Add files via upload

---
+++ Translating Javascript.py
@@ -0,0 +1,30 @@
+#        function mymin(a, b){
+#            if (a < b){
+#                return a;
+#            } else {
+#                return b;
+#            };
+#        }
+#        
+#        function square(x){
+#            return x * x;
+#        }
+#        
+#        write(mymin(square(-2), square(3)));
+
+
+def mymin(a,b):
+    
+    if a<b:
+        
+        return a
+    
+    else:
+        
+        return b
+
+def square(x):
+    
+    return x*x
+    
+print(mymin(square(-2), square(3)))
