6168f048c9bcdb866f2b9c455a0208681381103b
Arthur Shih <a1928375@hotmail.com>
2019-09-28 14:26:01 +0800

    Add files via upload

---
+++ Up To Ten.py
@@ -0,0 +1,17 @@
+def uptoten(x):
+    if x < 10:
+        return x
+    else:
+        return 10
+
+
+javascriptcode="""
+function uptoten(x) {
+    
+    if (x<10) {
+        return x;
+    }else{
+        return 10;
+    }
+}
+"""
